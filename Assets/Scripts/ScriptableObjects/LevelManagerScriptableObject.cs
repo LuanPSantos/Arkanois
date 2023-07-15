4b86b7e8e734458822de7280f95f19032a44df8b
Luan <santos.pereira.luan@gmail.com>
2023-07-14 22:31:24 -0300

    level flow

--- Assets/Scripts/ScriptableObjects/LevelManagerScriptableObject.cs
+++ Assets/Scripts/ScriptableObjects/LevelManagerScriptableObject.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
@@ -6,38 +7,28 @@ using UnityEngine.SceneManagement;
 [CreateAssetMenu]
 public class LevelManagerScriptableObject : ScriptableObject
 {
-
     [SerializeField]
-    private List<string> levelSceneNames = new List<string>();
+    private List<string> levelSceneNames;
 
     private int currentLevelIndex;
 
-    void Awake()
-    {
-        levelSceneNames.Add("Level1");
-        levelSceneNames.Add("Level2");
-    }
-
     public void LoadFistLevel()
     {
-        Debug.Log("LoadFistLevel");
         currentLevelIndex = 0;
         LoadScene(levelSceneNames[currentLevelIndex]);
     }
 
     public void OnGameRestarted()
     {
-        Debug.Log("OnGameRestarted");
         LoadScene(levelSceneNames[currentLevelIndex]);
     }
 
     public void OnLevelFinished()
     {
-        Debug.Log("OnLevelFinished");
         currentLevelIndex++;
         if(currentLevelIndex < levelSceneNames.Count)
         {
-            LoadScene(levelSceneNames[currentLevelIndex]);
+            WaitAndLoadScene();
         }
         else
         {
@@ -48,7 +39,12 @@ public class LevelManagerScriptableObject : ScriptableObject
 
     private void LoadScene(string name)
     {
-        Debug.Log("Loading scene "+ name);
         SceneManager.LoadScene(name, LoadSceneMode.Single);
+    }
+
+    private async Task WaitAndLoadScene()
+    {
+        await Task.Delay(500);
+        LoadScene(levelSceneNames[currentLevelIndex]);
     }
 }
